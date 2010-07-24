03de33dd6a1e738df2b49b0841e505b7b47901fd
deadprogrammer <ron.evans@gmail.com>
2010-07-23 17:58:31 -0700

    Rescue exceptions when reprocessing images

--- lib/paperclip/attachment.rb
+++ lib/paperclip/attachment.rb
@@ -219,6 +219,10 @@ module Paperclip
       else
         true
       end
+    rescue Exception => msg
+      # in the case of an exception, just add to activerecord base errors, and return so we can skip to the next one
+      add_to_base msg
+      return true
     end
 
     # Returns true if a file has been assigned.
